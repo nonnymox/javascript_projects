859a2e436bc6b72facde4eba858d53c58fed8817
nonnymox <knonxho@gmail.com>
2025-03-05 13:42:27 -0800

    Did the switch variable exercise

---
+++ variables.js
@@ -0,0 +1,22 @@
+var myName = "Nonso";
+alert("Hello " + myName)
+var userName = prompt("What is your name?")
+
+// Function to switch the value of two variables
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+    var c = a;
+    a= b
+    b = c 
+
+    
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
