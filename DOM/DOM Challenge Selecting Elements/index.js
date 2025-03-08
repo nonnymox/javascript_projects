1012713d68d440dadb8201b51214c81f196a9ce2
nonnymox <knonxho@gmail.com>
2025-03-08 09:27:13 -0800

    More on DOM manipulation

--- DOM/DOM Challenge Selecting Elements/index.js
+++ DOM/DOM Challenge Selecting Elements/index.js
@@ -1,7 +1,18 @@
-document.querySelector("ul li a").style.color = "red";
-document.querySelector("button").style.backgroundColor = "yellow";
-document.querySelector("button").classList.add("huge");
-document.querySelector("button").classList.remove("huge");
-for (var i = 1; i > 0; i--) {
-  document.querySelector("h1").classList.toggle("huge");
-}
+// document.querySelector("ul li a").style.color = "red";  //Selecting elements and changing style
+// document.querySelector("button").style.backgroundColor = "yellow";
+// document.querySelector("button").classList.add("huge"); // Adding element to a class
+// document.querySelector("button").classList.remove("huge"); // Removing element from a class
+// for (var i = 1; i > 0; i--) {
+//   document.querySelector("h1").classList.toggle("huge"); Toggling class on/off for selected element
+// }
+
+
+// document.querySelector("h1").innerText = "KnoXLeveliT"; 
+// document.querySelector("h1").textContent = "KnoXLeveliT"
+//Selecting the inner Text of an element
+
+document.querySelector("a").attributes; //Getting all attributes of an element
+
+document.querySelector("a").getAttribute("href"); // Getting the attribute specified
+
+document.querySelector("a").setAttribute("href", "https://knoxlevelit.com") //Setting the attribute of the element
