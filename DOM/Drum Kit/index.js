8d94392cf8766e4d698feca84871ac0bb6ea9685
nonnymox <knonxho@gmail.com>
2025-03-09 09:12:01 -0700

    Manipulated strings and dynamically play the drums on click

--- DOM/Drum Kit/index.js
+++ DOM/Drum Kit/index.js
@@ -1,18 +1,24 @@
 let buttons = document.querySelectorAll("button");
 
-// buttons.forEach((button) => {
-//   button.addEventListener("click", (event) => {
-//     var key = button.textContent;
-//     alert(`${key} was clicked`);
-//   });
-// });
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let bgImage = window.getComputedStyle(button).backgroundImage;
+    let imageUrl = bgImage.replace(/^url\(["']?|["']?\)$/g, ""); // Extracts only the URL
+    let fileName = imageUrl.split("/").pop(); // Splitting the url into parts
 
-for (let i = 0; i < buttons.length; i++) {
-  let button = buttons[i];
-  button.addEventListener("click", function () {
-    let key = button.innerText;
-    alert(`${key} was clicked`);
-    var audio = new Audio("crash.mp3");
+    let sound = fileName.split(".png").join(".mp3");
+    let audio = new Audio(`sounds/${sound}`);
+    audio.play();
+    // alert(`${key} was clicked`);
   });
-}
+});
 
+// for (let i = 0; i < buttons.length; i++) {
+//   let button = buttons[i];
+//   button.addEventListener("click", function () {
+//     let key = button.innerText;
+//     // alert(`${key} was clicked`);
+//     let audio = new Audio("sounds/tom-1.mp3");
+//     audio.play;
+//   });
+// }
