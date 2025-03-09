4d3293f0f18af848ec3ce4eb13a3ca67e3bec230
nonnymox <knonxho@gmail.com>
2025-03-08 22:13:30 -0800

    Started working on the drumkit js challenge

---
+++ DOM/Drum Kit/index.js
@@ -0,0 +1,16 @@
+let buttons = document.querySelectorAll("button");
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     var key = button.textContent;
+//     alert(`${key} was clicked`);
+//   });
+// });
+
+for (let i = 0; i < buttons.length; i++) {
+  let button = buttons[i];
+  button.addEventListener("click", function () {
+    let key = button.innerText;
+    alert(`${key} was clicked`);
+  });
+}
